b5297b79b96d6e96916ab599fa79ffb923a26bdc
KHP <61647777+karthik2522@users.noreply.github.com>
2020-06-01 17:30:56 +0530

    Update HeapDel.java

--- HeapDel.java
+++ HeapDel.java
@@ -2,9 +2,9 @@ public class HeapDel{
 
 	static void heapify(int arr[], int n, int i) 
 	{ 
-		int largest = i; // Initialize largest as root 
-		int l = 2 * i + 1; // left = 2*i + 1 
-		int r = 2 * i + 2; // right = 2*i + 2 
+		int largest = i; 
+		int l = 2 * i + 1; 
+		int r = 2 * i + 2; 
 		if (l < n && arr[l] > arr[largest]) 
 			largest = l; 
 
